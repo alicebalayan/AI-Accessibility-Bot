45ffdedf021ee2c63be4a746eee12b25330ce5e1
Andrew Nathenson <colorfulpockets01@gmail.com>
2021-01-30 12:50:21 -0800

    basic translation working

--- bot.py
+++ bot.py
@@ -15,11 +15,15 @@ import time
 
 TOKEN = open("token.txt").read()
 
-# This list is probably incomplete, but these are the words that just don't exist at all in ASL
-REMOVED_WORDS = ['a','an', 'the','is','am','be','are']
+# This list is probably incomplete, but these are the words that just don't exist at all in ASL/will more often than not be skipped
+REMOVED_WORDS = ['a','an', 'the','is','am','be','are', 'to']
 
-# TODO get this list of time-words
-TIME_WORDS = []
+time_words_file = open('time_words.txt', 'r')
+TIME_WORDS = time_words_file.read().split()
+
+PHRASE_ENDS = ['.',',',';',':']
+
+
 
 # custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 
@@ -32,15 +36,15 @@ def process_content(text):
 			# Not sure why, but the words need to be tokenized twice.  This is just how they did it in the tutorial I watched /shrug
 			word = nltk.word_tokenize(i)
 			tagged.append(nltk.pos_tag(word))
-		print(tagged)
+		#print(tagged)
 
 		pseudo_ASL = pseudo_translate(tagged)
+
+		print(pseudo_ASL)
 			
 	except Exception as e:
 		print(str(e))
 
-<<<<<<< HEAD
-=======
 # Takes a list of tagged words and does the following:
 #	1. Removes words that aren't used in ASL (articles such as "the" or "a", be verbs such as "be" and "am")
 #	2. Adds superlatives (in ASL, "biggest" could be signed as BIG + TOP).  
@@ -48,31 +52,48 @@ def process_content(text):
 #	3. Converts superlatives and comparatives to their roots.  For instance, "bigger" --> "big" TODO
 #	4. Checks for word pairs that have a single sign, such as "Good morning" or "Week last" TODO
 #	5. Changes some word ordering, specifically for:
-#		a. Time.  In ASL, timing words come first.  So rather than "I washed my car last week" it's LAST WEEK I WASH CAR
+#		a. Time.  In ASL, timing words come first.  So rather than "I washed my car last week" it's WEEK-LAST I WASH CAR
 #		b. Possibly other things? More research required
 def pseudo_translate(tagged):
 
 	# Remove unused words
 	tagged = [k for k in tagged if k[0][0].lower() not in REMOVED_WORDS]
-	print(tagged)
+	#print(tagged)
 	
 	# Add superlatives
 	words = []
 	for tag in tagged:
 		words.append(tag[0][0])
+		#print(tag[0][1])
 		if tag[0][1] == 'JJS':
 			words.append('top')
-	print(words)
 
 	# TODO convert superlatives and comparatives to their roots (ie "bigger" --> "big")
 
 	# TODO check for word pairs
 
-	# Change time ordering TODO
+	# Change time ordering 
+	for i in range(len(words)):
+		word = words[i]
+		if word in TIME_WORDS:
+			move_to_start_of_sentence(words, i)
+
+	# print(words)
 
 	return list(map(lambda x: x.upper(), words))
 
->>>>>>> translator
+# Takes list of words, and position of word to be moved
+def move_to_start_of_sentence(words, pos):
+	i = pos
+	while (i > 0):
+		if words[i] in PHRASE_ENDS:
+			word = words.pop(pos)
+			words.insert(i+1, word)
+			return
+		i -= 1
+	word = words.pop(pos)
+	words.insert(0, word)
+
 
 class MyClient(discord.Client):
 	async def on_ready(self):
