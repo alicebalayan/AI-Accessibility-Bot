c91b0bb636fabecd112b7d59920cf3dde27182e0
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-01-29 22:29:14 -0800

    put web sockets commented out

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import threading
 import asyncio
+import websockets
 from discord import channel
 import substring
 import random
@@ -13,29 +14,32 @@ from requests.sessions import session
 
 TOKEN = open("token.txt").read()
 
-
-		
-
-
 class MyClient(discord.Client):
-	
+	# async def socket(self,websocket,path):
+    #     # content=""
+    #     # while true:
+    #     #     await asyncio.sleep(2)
+    #     pass
+        
 	async def on_ready(self):
 		print(f'{self.user} has connected to Discord!')
 		print('Servers connected to:')
 		for server in client.guilds:
 			print(server)
-	
+        
+        # start_server = websockets.serve(self.socket, host=None,port=8765)
+
+        # asyncio.get_event_loop().run_until_complete(start_server)
+        # asyncio.get_event_loop().run_forever()
 	async def on_message(self,message):	
 		if message.author == self.user:
 			return
 		print(message.content)
-		
 	async def on_member_join(self,member):
 		print("new member joined")
+        
 	
 
 client = MyClient()
-
-
 
 client.run(TOKEN)
