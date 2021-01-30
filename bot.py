459a6558658a9861b690941b7307605e1a4751cf
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-01-29 21:11:02 -0800

    started bot

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+import threading
+import asyncio
+from discord import channel
+import substring
+import random
+import requests 
+from discord.ext import commands
+import aiohttp
+from io import BytesIO
+from requests.sessions import session
+
+
+TOKEN = open("token.txt").read()
+
+
+		
+
+
+class MyClient(discord.Client):
+	
+	async def on_ready(self):
+		print(f'{self.user} has connected to Discord!')
+		print('Servers connected to:')
+		for server in client.guilds:
+			print(server)
+	
+	async def on_message(self,message):	
+		if message.author == self.user:
+			return
+		print(message.content)
+		
+	async def on_member_join(self,member):
+		await member.create_dm()
+		await member.dm_channel.send(
+			f'Hi {member.name}, poopi welcomes you!'
+		)
+	
+
+client = MyClient()
+
+
+
+client.run(TOKEN)
